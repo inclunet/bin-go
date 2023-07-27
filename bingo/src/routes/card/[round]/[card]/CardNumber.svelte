3ca1f1a04033b49d7c9023d5742399ef2a8bf258
Edson Garcia <ednosmab@yahoo.com.br>
2023-07-27 19:32:34 -0300

    incluindo logo da inclunet na cartela do bingo

--- bingo/src/routes/card/[round]/[card]/CardNumber.svelte
+++ bingo/src/routes/card/[round]/[card]/CardNumber.svelte
@@ -11,8 +11,18 @@
     }
 </script>
 
-<button aria-pressed={number.Checked} on:click={checkNumber} class="btn active">{number.Number}</button>
+{#if number.Number != 0}
+<button aria-pressed={number.Checked} on:click={checkNumber} class="btn active">
+    {number.Number}
+</button>
+{:else}
+<img src="/favicon.png" alt="Logo da Inclunet">
+{/if}
 <style>
+    img{
+        width: 100px;
+        height: 100px;
+    }
     button[aria-pressed="true"]{
         background-color: #008000;
         color: white;
@@ -27,6 +37,10 @@
         font-size: 1.8em;
     }
     @media(max-width: 680px){
+        img{
+            width: 80px;
+            height: 80px;
+        }
         button{
             margin: 0.6em;
             width: 55px;
@@ -36,29 +50,32 @@
         }
     }
     @media(max-width: 470px){
+        img{
+            width: 75px;
+            height: 75px;
+        }
         button{
             margin: 0.5em;
             width: 50px;
             height: 50px;
-            font-weight: 900;
             font-size: 1.5em;
         }
     }
     @media(max-width: 400px){
         button{
             margin: 0.3em;
-            width: 50px;
-            height: 50px;
-            font-weight: 900;
             font-size: 1.3em;
         }
     }
     @media(max-width: 350px){
+        img{
+            width: 70px;
+            height: 70px;
+        }
         button{
             margin: 0.2em;
             width: 45px;
             height: 45px;
-            font-weight: 900;
             font-size: 1.1em;
         }
     }
