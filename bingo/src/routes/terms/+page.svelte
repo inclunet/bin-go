20f0b0a83d3c0037feef3e14f9417f24d7b390dd
Leonardo Gleison <leonardo.gleison@gmail.com>
2023-08-31 18:18:22 -0300

    Add term of use page

---
+++ bingo/src/routes/terms/+page.svelte
@@ -0,0 +1,31 @@
+<h2>Termos de Uso do IncluBingo</h2>
+
+<p>Bem-vindo ao IncluBingo! Antes de começar a usar nossos serviços, leia atentamente os seguintes Termos de Uso. Ao acessar ou utilizar o site IncluBingo, você concorda em cumprir e estar vinculado a estes termos. Se você não concorda com esses termos, por favor, não utilize o nosso site.</p>
+
+<h3>Uso do Site:</h3>
+
+<ol>
+  <li>O IncluBingo é um serviço online gratuito, construído com o propósito de oferecer oportunidade para pessoas com deficiência se divertirem em conjunto com outras pessoas sem deficiência, em conformidade com a legislação vigente. Fica estritamente proibida qualquer forma de exploração comercial deste site, conforme estabelecido pela Lei 9.981/2000 no Brasil.</li>
+</ol>
+
+<h3>Responsabilidade e Limitações:</h3>
+
+<ol start="2">
+  <li>O IncluBingo não se responsabiliza por quaisquer danos, perdas ou lesões resultantes do uso deste site ou da participação em jogos de bingo recreativos organizados por você.</li>
+</ol>
+
+<h3>Alterações nos Termos:</h3>
+
+<ol start="1">
+  <li>Reservamo-nos o direito de modificar ou atualizar estes Termos de Uso a qualquer momento. Quaisquer alterações serão efetivas imediatamente após a publicação das versões revisadas neste site.</li>
+</ol>
+
+<h3>Lei Aplicável:</h3>
+
+<ol start="1">
+  <li>Estes Termos de Uso serão regidos pelas leis locais do seu país de residência, sem levar em consideração os conflitos de disposições legais.</li>
+</ol>
+
+<p>Ao continuar a usar o IncluBingo, você concorda com estes Termos de Uso. Se você não concorda com eles, por favor, pare de usar o site. Obrigado por escolher o IncluBingo para sua diversão inclusiva!</p>
+
+<p>Última atualização: 21 de setembro de 2023</p>
