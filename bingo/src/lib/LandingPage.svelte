19da6a9869293bbd6fb2542760a31864a4455495
Leonardo Gleison <leonardo.gleison@gmail.com>
2023-08-31 11:03:20 -0300

    Adding a landing page to explain the game

---
+++ bingo/src/lib/LandingPage.svelte
@@ -0,0 +1,111 @@
+<div>
+    <h2>Bem-vindo ao IncluBingo: O Jogo de Bingo Acessível e Inclusivo!</h2>
+    <strong> Diversão Inclusiva ao Seu Alcance! </strong>
+    <p>Explore a emoção de um bingo acessível para todos no IncluBingo.</p>
+    <p>
+        Como um sserviço criado para unir pessoas com diferentes capacidades,
+        criamos uma experiência de jogo que promove a inclusão e a diversão para <strong
+            >todos</strong
+        >.
+    </p>
+
+    <div>
+        <h3>Principais Recursos</h3>
+
+        <div>
+            <h4>Marcação Automática</h4>
+            <p>
+                Desfrute do jogo sem a preocupação de marcar os números
+                sorteados. Nosso jogo pode cuidar disso para você, permitindo
+                que todos se concentrem no momento, mas se você preferir marcar
+                os números manualmente, você pode fazer isso também.
+            </p>
+        </div>
+        <div>
+            <h4>Avisos Visuais e Sonoros</h4>
+            <p>
+                Com alertas visuais e sonoros, todos podem comemorar quando
+                alguém completa um bingo. A vitória é compartilhada e celebrada
+                por todos.
+            </p>
+        </div>
+
+        <div>
+            <h4>Acessilidade Aprimorada</h4>
+            <p>
+                Com informações visuais e auditivas, o IncluBingo oferece
+                igualdade de oportunidades. Jogadores cegos podem participar
+                plenamente, tornando a diversão verdadeiramente inclusiva.
+            </p>
+        </div>
+
+        <div>
+            <h4>Escolha a Velocidade do Jogo</h4>
+            <p>
+                É possível iniciar rodadas de 75 ou 90 bolinhas, permitindo
+                escolher um ritmo de jogo mais interessante para você e seus
+                amigos. Personalize sua experiência de jogo de acordo com suas
+                preferências.
+            </p>
+        </div>
+
+        <div>
+            <h4>Ofereça Prêmios para Rodadas Intermediárias!</h4>
+            <p>
+                Com as rodadas intermediárias você pode prmiar quem Completa
+                linhas horizontais, verticais ou diagonais. O IncluBingo eleva a
+                diversão a outro nível.
+            </p>
+        </div>
+
+        <div>
+            <h4>QR-Code para Sorteio</h4>
+            <p>
+                Utilize nosso QR-Code para sortear novas cartelas de forma
+                rápida e eficiente. A tecnologia moderna torna o jogo ainda mais
+                acessível.
+            </p>
+        </div>
+
+        <div>
+            <h4>Compartilhamento Simplificado</h4>
+            <p>
+                Compartilhe suas cartelas com um único clique em redes sociais
+                como WhatsApp e outras. Convide amigos e familiares para se
+                juntarem à diversão inclusiva.
+            </p>
+        </div>
+
+        <div>
+            <h4>O Jogo não Para!</h4>
+            <p>
+                É possível jogar sem interrupções! Ative a função de
+                redirecionamento automático para uma experiência de jogo fluida.
+            </p>
+        </div>
+
+        <div>
+            <h4>Configuração Personalizada</h4>
+            <p>
+                Defina a quantidade de rodadas intermediárias válidas para cada
+                partida. Adapte o jogo de acordo com o seu grupo e preferências.
+            </p>
+        </div>
+    </div>
+    <div>
+        Pronto para Jogar? Vamos Começar! Escolha a sua velocidade, personalize
+        suas opções e convide amigos e familiares para participar desta jornada
+        inclusiva no IncluBingo. A diversão está apenas a um clique de
+        distância.
+
+        <a href="/card/new">Jogue Agora e Faça Parte da Diversão Inclusiva!</a>
+    </div>
+
+    <div>
+        <p>
+            O IncluBingo está comprometido em fornecer uma experiência de jogo
+            segura e responsável para todos os seus usuários. Respeite todas as
+            leis e regulamentos locais ao usar nosso site.
+        </p>
+    </div>
+</div>
---
+++ bingo/src/routes/card/new/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    import NewRound from "$lib/NewRound.svelte";
+    export let data;
+    export let card = data;
+</script>
+
+<div class="container-fluid d-flex align-items-center flex-column">
+    
+    <h2 class="text-center">Bem-Vindo ao Inclubingo</h2>
+    <p class="text-center">Vamos Jogar! Escolha a quantidade de bolinhas que serão sorteadas:</p>
+    <NewRound bind:card/>
+</div>
+
+<style>
+    h2{
+        margin-top: 50px;
+        font-size: 2.5em;
+    }
+    p{
+        margin-top: 25px;
+        padding: 0 10px 0 10px;
+        font-size: 1.6em;
+    }
+    @media(max-width: 450px){
+        h2{
+            margin: 0;
+            padding: 10px 0 10px 0;
+            font-size: 1.5em;
+        }
+        p{
+            margin: 0;
+            margin-bottom: 20px;
+            font-size: 1em;
+        }
+    }
+</style>
