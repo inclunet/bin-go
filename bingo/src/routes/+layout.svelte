e384c49752a20b60df728c0aa40a90898fcdea98
Edson Garcia <ednosmab@yahoo.com.br>
2023-09-06 15:54:32 -0300

    Adjusting font size and including logo

--- bingo/src/routes/+layout.svelte
+++ bingo/src/routes/+layout.svelte
@@ -1,20 +1,29 @@
-<header>
-    <h1 class="text-center">Inclubingo</h1>
+<header class="d-flex flex-row">
+    <a href="/"><img src="/img/logo.png" alt="Logotipo da Inclunet"></a>
+    <h1 class="text-center ">Inclubingo</h1>
 </header>
 <main>
     <slot></slot>
 </main>
 <footer>
-    <p class="text-center">Jogo de Bingo acessível e inclusivo para pessoas com deficiência.</p>
+    <p class="text-center">
+        <strong>
+            Jogo de Bingo acessível e inclusivo para pessoas com deficiência.
+        </strong>
+    </p>
 </footer>
 
 <style>
-   
-    h1{
-        padding-top: 50px;
-        padding-bottom: 20px;
+   header{
+       background-color: #2b7ef4;
+       padding: 50px 50px 20px 50px;
+       align-items: center;
+   }
+   img{
+   }
+    header h1{
+        width: 100%;
         font-size: 4.4em;
-        background-color: #2b7ef4;
         color: #000;
     }
     p{
@@ -23,12 +32,26 @@
         font-size: 1.6em;
     }
     @media(max-width: 450px){
-        h1{
-            padding: 0;
+        header{
+            padding: 0 10px 5px 10px;
+            justify-content: space-between;
+            /* align-items: baseline; */
+        }
+        header img{
+            /* border: 1px solid black; */
+            width: 80%;
+        }
+        header h1{
+            /* border: 1px solid black; */
+            height: auto;
+            margin-top: 15px;
+            /* padding: 15px 0 0 0; */
             font-size: 2.5em;
         }
         p{
-            font-size: 1em;
+            font-size: 1.3em;
         }
     }
 </style>
+
+<!-- commit -m "Adjusting font size and including logo" -->
