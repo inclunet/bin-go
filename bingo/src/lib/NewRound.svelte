fcf07685366c7d024d704b9cf08f0840923ccf79
Edson Garcia <ednosmab@yahoo.com.br>
2023-09-12 16:59:14 -0300

    refactoring style button

--- bingo/src/lib/NewRound.svelte
+++ bingo/src/lib/NewRound.svelte
@@ -16,14 +16,27 @@
 </script>
 
 {#if (card.Card == 1 && card.Checked == card.Type) || card.Round == 0}
-    <button class="btn btn-primary" on:click={newRound}><strong>Nova Rodada</strong></button>
+    <button class="btn" on:click={newRound}><strong>Nova Rodada</strong></button>
 {/if}
 
 <style>
+    :root{
+        --primary-button-color: #2b7ef4;
+        --secondary-button-color: #2868c2;
+        --tertiary-button-color: #2157a1;
+    }
     button{
         padding: 10px;
         font-size: 1.3em;
-        color: #000;
+        background-color: var(--primary-button-color);
+    }
+    button:hover{
+        color: #fff;
+        background-color: var(--secondary-button-color);
+    }
+    .btn:active{
+        color: #fff;
+        background-color: var(--tertiary-button-color);
     }
     @media(max-width: 450px){
         button{
