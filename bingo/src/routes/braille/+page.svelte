4104934c07363ee6616871c95624496ef5a44e79
Edson Garcia <ednosmab@yahoo.com.br>
2024-05-10 17:45:38 -0300

    organizing file structures

--- bingo/src/routes/braille/+page.svelte
+++ bingo/src/routes/braille/+page.svelte
@@ -2,24 +2,100 @@
     import { goto } from "$app/navigation";
     import PageTitle from "$lib/PageTitle.svelte";
 
-const handleStartLearningNow = () => {
-goto(`/braille/new`);
-};
+    const handleStartLearningNow = () => {
+        goto(`/braille/new`);
+    };
 </script>
 
-<PageTitle title="Braille Personal Trainer" />
+<PageTitle title="Braille Personal Trainer" game="Inclubraille" />
 
-<h2>Braille Personal Trainer</h2>
+<div class="container">
+    <header class="my-5">
+        <h2>Braille Personal Trainer</h2>
+    </header>
 
-<p>
-    O Braille Personal Trainer é um aplicativo que ajuda a treinar a escrita em Braille. O aplicativo é composto por
-    exercícios de escrita em Braille e de escrita em tinta. O aplicativo também possui um teclado Braille virtual para
-    auxiliar na escrita.
-</p>
+    <section>
+        <p>
+            O Braille Personal Trainer é um aplicativo que ajuda a treinar a
+            escrita em Braille. O aplicativo é composto por exercícios de
+            escrita em Braille e de escrita em tinta. O aplicativo também possui
+            um teclado Braille virtual para auxiliar na escrita.
+        </p>
 
-<p>
-    O aplicativo é composto por aulas de escrita em Braille. Cada aula é composta por exercícios de escrita em Braille e
-    de escrita em tinta. O aplicativo também possui um teclado Braille virtual para auxiliar na escrita.
-</p>
+        <p>
+            O aplicativo é composto por aulas de escrita em Braille. Cada aula é
+            composta por exercícios de escrita em Braille e de escrita em tinta.
+            O aplicativo também possui um teclado Braille virtual para auxiliar
+            na escrita.
+        </p>
+    </section>
 
-<button on:click={handleStartLearningNow}>Comece a aprender Agora</button>
+    <button
+        class="button-color btn text-black"
+        on:click={handleStartLearningNow}>Comece a aprender Agora</button
+    >
+</div>
+
+<style>
+    :root {
+        --primary-button-color: #2b7ef4;
+    }
+    h2:nth-child(2n) {
+        font-size: 1.6em;
+    }
+
+    section p {
+        font-size: 1.2em;
+    }
+
+    p {
+        line-height: 32px;
+    }
+
+    h3 {
+        font-size: 1.4em;
+    }
+
+    .term {
+        padding: 3px 5px 3px 5px;
+        margin: 0;
+        font-size: 1em;
+    }
+    .button-color {
+        background-color: var(--primary-button-color);
+    }
+
+    @media (max-width: 573px) {
+        .container {
+            padding: 0 20px 0 20px;
+        }
+        header h2 {
+            margin-top: -20px;
+            line-height: 40px;
+            text-align: center;
+        }
+        h3 {
+            line-height: 40px;
+        }
+        header p:nth-child(n + 3):nth-child(-n + 4) {
+            text-align: justify;
+        }
+        .row {
+            padding: 0px;
+        }
+        .card {
+            border-radius: 10px;
+            box-shadow: 5px 5px 5px #e2e2e2;
+        }
+
+        .main-features-img {
+            margin-left: 10px;
+        }
+        p {
+            line-height: 37px;
+        }
+        section p {
+            text-align: justify;
+        }
+    }
+</style>
--- bingo/src/routes/card/+page.svelte
+++ bingo/src/routes/card/+page.svelte
@@ -3,16 +3,16 @@
     import PageTitle from "$lib/PageTitle.svelte";
 </script>
 
-<PageTitle title="Início" />
-    <div class="m-3 me-5 term-use">
-        <a href="/terms" class="btn btn-warning btn-lg">Termos de uso</a>
-    </div>
+<PageTitle title="Início" game="Inclubingo" />
+<div class="m-3 me-5 term-use">
+    <a href="/terms" class="btn btn-warning btn-lg">Termos de uso</a>
+</div>
 
 <LandingPage />
 
 <style>
-    @media(max-width: 450px){
-        .term-use{
+    @media (max-width: 450px) {
+        .term-use {
             display: none;
         }
     }
