a56ae9198f5d7c5aeb6999d10683e1b76cc04cf5
Edson Garcia <ednosmab@yahoo.com.br>
2024-06-15 21:08:47 -0300

    creating a card play component and home page

---
+++ bingo/src/lib/CardPlay.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let title;
+    export let titleButton;
+    export let href;
+</script>
+
+<div class="card">
+    <img src="..." class="card-img-top" alt="..." />
+    <div class="card-body">
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">
+            <slot></slot>
+        </p>
+        <a {href} class="btn btn-primary">{titleButton}</a>
+    </div>
+</div>
+
+<style>
+    .card {
+        width: 30rem;
+        margin: 2rem;
+    }
+    .card-title {
+        font-size: 2.5rem;
+    }
+    .card-body p {
+        font-size: 1.7rem;
+    }
+    .card-body a {
+        font-size: 1.6rem;
+    }
+</style>
--- bingo/src/routes/+page.svelte
+++ bingo/src/routes/+page.svelte
@@ -1,1 +1,57 @@
-<h1>Home</h1>
+<script>
+    import CardPlay from "$lib/CardPlay.svelte";
+    import Footer from "$lib/Footer.svelte";
+</script>
+
+<header>
+    <img src="/img/inclunet.png" alt="Inclunet" />
+</header>
+<main class="container">
+    <slot>
+        <div class="container-card">
+            <CardPlay
+                href="/bingo"
+                title="Jogo de Bingo Inclusivo"
+                titleButton="Inclubingo"
+            >
+                Explore a emoção de um bingo acessível para todos no IncluBingo.
+            </CardPlay>
+            <CardPlay
+                href="/braille"
+                title="Braille Personal Trainer"
+                titleButton="Inclubraille"
+            >
+                O Braille Personal Trainer te deixará expert na escrita em
+                Braille
+            </CardPlay>
+        </div>
+    </slot>
+</main>
+<Footer>Aprenda Braille brincando.</Footer>
+
+<style>
+    :root {
+        --primary-color: #2b7ef4;
+        --secondary-color: #2b7ff4ab;
+        --tertiary-color: #982a35;
+        --quaternary-color: #fff;
+        --quinary-color: #000;
+        --senary-color: #1d1d1d;
+        --setenary-color: #5799f5;
+        --octonary-color: #006aff;
+
+        font-size: 62.5%;
+    }
+
+    header img {
+        width: 100%;
+    }
+    .container {
+        margin-top: 4rem;
+    }
+    .container-card {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+</style>
