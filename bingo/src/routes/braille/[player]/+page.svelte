77c18421ffb601900a9dd6e87668bfb5b6ad3612
Edson Garcia <ednosmab@yahoo.com.br>
2025-02-09 11:08:25 -0300

    adjusting button size and top spacing

--- bingo/src/routes/braille/[player]/+page.svelte
+++ bingo/src/routes/braille/[player]/+page.svelte
@@ -56,13 +56,22 @@
         font-size: 62.5%;
     }
 
+    .container {
+        min-height: 60vh;
+    }
+
     h2 {
         font-size: 2.8rem;
     }
 
-    section p,
-    section button {
+    section p {
         font-size: 1.8rem;
+    }
+
+    section button {
+        margin-top: 5rem;
+        font-size: 2rem;
+        padding: 1rem 2rem;
     }
 
     p {
@@ -74,15 +83,23 @@
         color: var(--black);
     }
 
-    .button-color:hover {
+    .button-color:active {
         background-color: var(--secondary-button-color);
         color: var(--white);
+        border: 1px solid var(--black);
     }
 
     .term {
         padding: 3px 5px 3px 5px;
         margin: 0;
         font-size: 1em;
+    }
+
+    @media (hover: hover) {
+        .button-color:hover {
+            background-color: var(--secondary-button-color);
+            color: var(--white);
+        }
     }
 
     @media (max-width: 573px) {
--- bingo/src/routes/braille/new/+page.svelte
+++ bingo/src/routes/braille/new/+page.svelte
@@ -37,11 +37,16 @@
         font-size: 62.5%;
     }
 
+    .container {
+        min-height: 60vh;
+    }
     h2 {
         font-size: 2.8rem;
     }
     section button {
-        font-size: 1.8rem;
+        margin-top: 5rem;
+        font-size: 2rem;
+        padding: 1rem 2rem;
     }
 
     p {
@@ -52,17 +57,24 @@
         background-color: var(--primary-button-color);
         color: var(--black);
     }
-
-    .button-color:hover {
+    .button-color:active {
         background-color: var(--secondary-button-color);
         color: var(--white);
+        border: 1px solid var(--black);
     }
+
     .term {
         padding: 3px 5px 3px 5px;
         margin: 0;
         font-size: 1em;
     }
 
+    @media (hover: hover) {
+        .button-color:hover {
+            background-color: var(--secondary-button-color);
+            color: var(--white);
+        }
+    }
     @media (max-width: 573px) {
         .container {
             padding: 0 20px 0 20px;
