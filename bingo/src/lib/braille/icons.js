d958940ae573a7c0c99da559ce1ce7544c1db83e
Edson Garcia <ednosmab@yahoo.com.br>
2025-03-16 19:20:05 -0300

    add news icons

--- bingo/src/lib/braille/icons.js
+++ bingo/src/lib/braille/icons.js
@@ -1,7 +1,38 @@
-// Icons list in https://icons.getbootstrap.com/icons/
+// Icons list in https://icons.getbootstrap.com/
 export const HelpIcon = (size = 16) => {
     return `
         <svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}" fill="currentColor" class="bi bi-question-lg" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M4.475 5.458c-.284 0-.514-.237-.47-.517C4.28 3.24 5.576 2 7.825 2c2.25 0 3.767 1.36 3.767 3.215 0 1.344-.665 2.288-1.79 2.973-1.1.659-1.414 1.118-1.414 2.01v.03a.5.5 0 0 1-.5.5h-.77a.5.5 0 0 1-.5-.495l-.003-.2c-.043-1.221.477-2.001 1.645-2.712 1.03-.632 1.397-1.135 1.397-2.028 0-.979-.758-1.698-1.926-1.698-1.009 0-1.71.529-1.938 1.402-.066.254-.278.461-.54.461h-.777ZM7.496 14c.622 0 1.095-.474 1.095-1.09 0-.618-.473-1.092-1.095-1.092-.606 0-1.087.474-1.087 1.091S6.89 14 7.496 14"/>
         </svg>`;
+};
+
+export const PointsIcon = (size = 16) => {
+    return `
+        <svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size} fill="currentColor" class="bi bi-bar-chart-line-fill" viewBox="0 0 16 16">
+          <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1z"/>
+        </svg>`;
+};
+
+export const TrophyIcon = (size = 16, color = "black") => {
+    return `
+      <svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size} class="bi bi-trophy-fill" viewBox="0 0 16 16">
+        <style type="text/css">
+          .fill-color {fill: ${color};}
+        </style>
+
+        <path class="fill-color" d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5q0 .807-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33 33 0 0 1 2.5.5m.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935m10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935"/>
+      </svg>`;
+};
+
+export const StarIcon = (size = 16, color = "black") => {
+    return `
+      
+      
+      <svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size} fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
+        <style type="text/css">
+          .fill-color {fill: ${color};}
+        </style>
+
+  <path class="fill-color" d="M5.354 5.119 7.538.792A.52.52 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.54.54 0 0 1 16 6.32a.55.55 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.5.5 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.6.6 0 0 1 .085-.302.51.51 0 0 1 .37-.245zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.56.56 0 0 1 .162-.505l2.907-2.77-4.052-.576a.53.53 0 0 1-.393-.288L8.001 2.223 8 2.226z"/>
+</svg>`;
 };
