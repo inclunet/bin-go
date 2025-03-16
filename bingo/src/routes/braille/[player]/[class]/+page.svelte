838372b06021955b765ad4e2025f99518110e271
Edson Garcia <ednosmab@yahoo.com.br>
2025-03-16 19:25:01 -0300

    add progress bar and include TrophyIcon

--- bingo/src/routes/braille/[player]/[class]/+page.svelte
+++ bingo/src/routes/braille/[player]/[class]/+page.svelte
@@ -3,9 +3,8 @@
     import BrailleChallenge from "$lib/BrailleChallenge.svelte";
     import PageTitle from "$lib/PageTitle.svelte";
     import { callApi } from "$lib/api.js";
-    import Card from "$lib/bingo/Card.svelte";
     import { braille } from "$lib/braille.js";
-    import CardPoints from "$lib/braille/CardPoints.svelte";
+    import { StarIcon, TrophyIcon } from "$lib/braille/icons.js";
     import { onMount } from "svelte";
 
     export let data;
@@ -60,6 +59,18 @@
         }
     };
 
+    let progress = 0;
+
+    function progressClass() {
+        return Math.round(
+            (($braille.CurrentRound - 0) / ($braille.TotalRounds - 0)) * 100
+        );
+    }
+
+    $: if ($braille.CurrentRound > 0) {
+        progress = progressClass();
+    }
+
     onMount(LoadBrailleClass);
 </script>
 
@@ -69,51 +80,30 @@
 />
 
 <div class="container">
-    <!-- <header class="my-5">
-        <h2>Aula #{$braille.CurrentClass} - Braille Personal Trainer</h2>
-    </header> -->
-
-    <!-- <section>
-        <p class="paragraph-description">{$braille.Description}</p>
-    </section> -->
+    <div class="container_progress">
+        <div
+            class="progress"
+            role="progressbar"
+            aria-label="Progresso da rodada em {progress}%"
+            aria-valuenow={$braille.CurrentRound}
+            aria-valuemin="0"
+            aria-valuemax={$braille.TotalRounds}
+        >
+            <div
+                class="progress-bar progress-bar-striped bg-info text-dark"
+                style="width: {progress}%;"
+            >
+                {progress}%
+            </div>
+        </div>
+        {@html TrophyIcon(30, "gold")}
+    </div>
 
     <section class="container_play">
         <BrailleChallenge
             bind:brailleChallenge={$braille.Challenge}
             on:submitChallenge={handleChallengeSubmit}
         />
-    </section>
-
-    <section>
-        <h2 class="hidden-area">
-            Nesta área contém a série atual, rodada e pontos
-        </h2>
-        <div class="container_card">
-            <CardPoints
-                pointBackgroundColor="blue"
-                pointValue={$braille.CurrentClass + 1}
-                pointTitle="Série"
-                pointTitleHidden="Série"
-            />
-            <CardPoints
-                pointBackgroundColor="orange"
-                pointValue={$braille.CurrentRound}
-                pointTitle="Rodada"
-                pointTitleHidden="Pontos da Rodada"
-            />
-            <CardPoints
-                pointBackgroundColor="green"
-                pointValue={$braille.CurrentPunctuation}
-                pointTitle="Exercício"
-                pointTitleHidden="Pontos do exercício"
-            />
-            <CardPoints
-                pointBackgroundColor="purple"
-                pointValue={$braille.TotalPunctuation}
-                pointTitle="Total"
-                pointTitleHidden="Total de Pontos"
-            />
-        </div>
     </section>
 </div>
 
@@ -123,40 +113,29 @@
 <audio bind:this={startClassAudio} src="/startclass.wav"></audio>
 
 <style>
-    :root {
-        --primary-button-color: #2b7ef4;
-        --secondary-button-color: #2868c2;
-        --white: #fff;
-        --black: #000;
-
-        font-size: 62.5%;
+    .container_progress {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: 1.5rem;
     }
 
+    .progress {
+        width: 96%;
+        height: 2rem;
+        border-radius: 10px;
+    }
+
+    .progress-bar {
+        font-size: 1.8rem;
+        font-weight: 500;
+    }
     .container {
         min-height: 100vh;
     }
 
-    h2 {
-        font-size: 2.8rem;
-    }
-
-    .container_card {
-        margin: 5rem 0;
-        display: flex;
-        justify-content: space-between;
-    }
-    .paragraph-description {
-        font-size: 1.8rem;
-    }
-
-    .paragraph-description {
-        line-height: 4rem;
-    }
-
     @media (max-width: 490px) {
-        .container_card {
-            margin: 0;
-        }
         .container_play {
             min-height: 110vw;
         }
@@ -165,23 +144,6 @@
     @media (max-width: 470px) {
         .container {
             padding: 0 20px 0 20px;
-        }
-        header h2 {
-            margin-top: -20px;
-            line-height: 4rem;
-            text-align: center;
-        }
-
-        p {
-            line-height: 4.7rem;
-        }
-        section p {
-            text-align: justify;
-        }
-
-        .container_card {
-            /* flex-wrap: wrap; */
-            /* agrupa em dois cards por linha no total de duas linhas */
         }
     }
 </style>
