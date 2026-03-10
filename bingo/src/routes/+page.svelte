b640ae7e240a778677a2aadd35c4b97e4d564237
Edson Garcia <ednosmab@yahoo.com.br>
2026-03-09 22:23:57 -0300

    image name change

--- bingo/src/routes/+page.svelte
+++ bingo/src/routes/+page.svelte
@@ -1,63 +1,64 @@
 <script>
-    import PageTitle from "$lib/PageTitle.svelte";
-    import CardPlay from "$lib/CardPlay.svelte";
-    import Footer from "$lib/Footer.svelte";
+	import PageTitle from "$lib/PageTitle.svelte";
+	import CardPlay from "$lib/CardPlay.svelte";
+	import Footer from "$lib/Footer.svelte";
 </script>
 
 <PageTitle title="Inclunet" game="Homepage" />
 <header>
-    <img src="/img/inclunet.png" alt="Inclunet" />
+	<img src="/img/inclunet.png" alt="Inclunet" />
 </header>
 <main class="container">
-    <slot>
-        <div class="container-card">
-            <CardPlay
-                src="/img/chamada_card_inclubingo.png"
-                alt="Inclubingo"
-                href="/bingo"
-                title="Jogo de Bingo Inclusivo"
-            >
-                Explore a emoção de um bingo acessível para todos no IncluBingo.
-            </CardPlay>
-            <CardPlay
-                src="/img/chamada_card_inclubraille.png"
-                alt="Inclubraille"
-                href="/braille"
-                title="Braille Personal Trainer"
-            >
-                O Braille Personal Trainer te deixará expert na escrita em
-                Braille.
-            </CardPlay>
-            <CardPlay
-                src="/img/jogo2.png"
-                alt="Jogo da Velha Inclusivo"
-                href="/tictac"
-                title="Jogo da Velha Inclusivo"
-            >
-                Partidas rápidas, acessíveis e em tempo real. Desafie um amigo agora.
-            </CardPlay>
-        </div>
-    </slot>
+	<slot>
+		<div class="container-card">
+			<CardPlay
+				src="/img/chamada_card_inclubingo.png"
+				alt="Inclubingo"
+				href="/bingo"
+				title="Jogo de Bingo Inclusivo"
+			>
+				Explore a emoção de um bingo acessível para todos no IncluBingo.
+			</CardPlay>
+			<CardPlay
+				src="/img/chamada_card_inclubraille.png"
+				alt="Inclubraille"
+				href="/braille"
+				title="Braille Personal Trainer"
+			>
+				O Braille Personal Trainer te deixará expert na escrita em
+				Braille.
+			</CardPlay>
+			<CardPlay
+				src="/img/chamada_card_tic-tac.png"
+				alt="Jogo da Velha Inclusivo"
+				href="/tictac"
+				title="Jogo da Velha Inclusivo"
+			>
+				Partidas rápidas, acessíveis e em tempo real. Desafie um amigo
+				agora.
+			</CardPlay>
+		</div>
+	</slot>
 </main>
 <Footer />
 
 <style>
-    header img {
-        width: 100%;
-    }
-    .container {
-        min-height: auto;
-        margin-top: 4rem;
-    }
-    .container-card {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-    }
+	header img {
+		width: 100%;
+	}
+	.container {
+		min-height: auto;
+		margin-top: 4rem;
+	}
+	.container-card {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+	}
 
-    @media (max-width: 400px) {
-        .container-card {
-            justify-content: center;
-        }
-    }
+	@media (max-width: 400px) {
+		.container-card {
+			justify-content: center;
+		}
+	}
 </style>
